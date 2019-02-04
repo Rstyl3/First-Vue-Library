1205b9d11a6f455c9f41d37d268fc9c4e2aa407c
Severin Haselmann <32898792+Rstyl3@users.noreply.github.com>
2019-02-04 14:11:11 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import XfChatBox from '@/components/XfChatBox.vue'
+
+Vue.component('xf-chat-box',  XfChatBox )
 
 Vue.config.productionTip = false
 
